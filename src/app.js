1bb6cb101fc671efaa5cf4ac5e3ef1065fc97167
miyi <mylegojesus@gmail.com>
2016-10-17 16:00:58 -0700

    up to 7, but trying out webpack

--- src/app.js
+++ src/app.js
@@ -1,15 +1,161 @@
-var React  = require('react');
-var ReactDOM = require('react-dom');
+//var data = require(./data/data.js);
+
 
 var BugList = React.createClass({
+    getInitialState: function() {
+        return {bugs:[]}
+    },
+    loadBugs: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({bugs: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+
+        // $.ajax('/api/bugs').done(function(bugs) {
+        //     this.setState({bugs:bugs});
+        // }.bind(this));
+    },
+
+    componentDidMount: function() {
+            this.loadBugs();
+    },
+    render: function() {
+        console.log("Rendering bug list, num items:", this.state.bugs.length);
+        return (
+            <div>
+                <h1>Bug Tracker</h1>
+                <BugFilter />
+                <hr />
+                <BugTable bugs = {this.state.bugs}/>
+                <hr />
+                <BugSubmitForm addBug = {this.addBug}/>
+            </div>
+        )
+    },
+
+    addBug: function(newBugJSON) {
+        console.log('adding a new bug: ', newBugJSON);
+
+        //TODO: optimistic updates
+
+        $.ajax({
+            type: 'post',
+            url: this.props.url,
+            contentType: 'application/JSON',
+            data: JSON.stringify(newBugJSON),
+            success: function(data) {
+                var newBug = data;
+                var bugsModified = this.state.bugs.concat(newBug);
+                //concat is the preferred way to mutate a react state, but react also has an "immutability helper'
+                this.setState({bugs:bugsModified});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+        // We're advised not to modify the state directly, it's immutable. So, make a copy and setState with the copy
+        // var bugsModified = this.state.bugs.slice();
+        // newBugJSON.id = this.state.bugs.length + 1;
+        // bugsModified.push(newBugJSON);
+        // this.setState({bugs: bugsModified});
+
+    }
+});
+
+var BugFilter = React.createClass({
     render: function() {
         return (
-            <div>bugs will be shown here</div>
+            <div>A way to filter the list of bugs would come here.</div>
         )
     }
 });
 
+var BugRow = React.createClass({
+    render: function() {
+        console.log("Rendering BugRow:", this.props.bug);
+        return(
+            <tr>
+                <td>{this.props.bug._id}</td>
+                <td>{this.props.bug.status}</td>
+                <td>{this.props.bug.priority}</td>
+                <td>{this.props.bug.owner}</td>
+                <td>{this.props.bug.title}</td>
+            </tr>
+        )
+    }
+});
+
+var BugTable = React.createClass({
+    render: function() {
+        console.log("Rendering bug table, num items:", this.props.bugs.length);
+        var bugRows = this.props.bugs.map(function(bug) {
+
+            return <BugRow key={bug._id} bug={bug}/>
+
+        });
+
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Status</th>
+                            <th>Priority</th>
+                            <th>Owner</th>
+                            <th>Title</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {bugRows}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+});
+
+var BugSubmitForm = React.createClass({
+
+    submitToAddBug: function(e) {
+        e.preventDefault();
+        console.log('handling submit');
+
+        //document.forms.formname has to be the form name of the form I'm trying to submit under name attribute
+        var form = document.forms.bugForm;
+
+
+        // TODO: send req to server
+        this.props.addBug({owner: form.owner.value, title: form.title.value, status: form.status.value, priority: form.priority.value});
+        form.owner.value = ""; form.title.value = ""; form.status.value = ""; form.priority.value = "";
+
+
+    },
+
+    render: function() {
+        console.log('Rendering Bug Submit Form');
+        return(
+            <div>
+                <form name="bugForm">
+                    <input type="text" name="owner" placeholder="Owner" />
+                    <input type="text" name="title" placeholder="Title" />
+                    <input type="text" name="status" placeholder="Status" />
+                    <input type="text" name="priority" placeholder="priority" />
+                    <button onClick={this.submitToAddBug}>Add Bug</button>
+                </form>
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <BugList />,
+    <BugList url='/api/bugs'/>,
     document.getElementById('main')
 );
--- webapp.js
+++ webapp.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+//hashnode uses MongoClient
+var MongoClient = require('mongodb').MongoClient;
+//var test = require('assert');
 var port =  process.env.PORT || 3000;
+
+//TODO: create separate routes
+//TODO: use mongoose for db
 
 //m'routes ** lets not use m'routes
 //var routes = require('./app/routes.js');
@@ -8,8 +15,35 @@ var port =  process.env.PORT || 3000;
 
 //express instance
 var app = express();
+var db;
+
+//Connect to Mongo via MongoClient
+var url = 'mongodb://localhost/bugsdb';
+MongoClient.connect(url, function(err, dbConnection) {
+    db = dbConnection;
+    console.log("Connected correctly to server.");
+});
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+//GET
+app.get('/api/bugs', function(req, res) {
+    db.collection('bugs').find().toArray(function(err, docs) {
+        res.json(docs);
+    })
+});
+//POST 5.2 testing
+app.post('/api/bugs', function(req, res) {
+    console.log('Req Body: ', req.body);
+    var newBug = req.body;
+    db.collection('bugs').insertOne(newBug, function(err, result) {
+        var newBugID = result.insertedId;
+        db.collection('bugs').find({ _id: newBugID}).next(function(err, doc) {
+            res.json(doc);
+        });
+    });
+});
 
 //listening on port
 app.listen(port);
